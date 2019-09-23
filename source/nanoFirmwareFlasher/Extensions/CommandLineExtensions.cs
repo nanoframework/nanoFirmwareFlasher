9bfca2ad78a072f73a693cc0108d891e990d81a1
Søren Bech Christensen <sbc@lodam.com>
2019-09-23 18:41:05 +0200

    WithNotParsedAsync added

--- source/nanoFirmwareFlasher/Extensions/CommandLineExtensions.cs
+++ source/nanoFirmwareFlasher/Extensions/CommandLineExtensions.cs
@@ -5,6 +5,7 @@
 
 using CommandLine;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 
@@ -34,5 +35,42 @@ namespace nanoFramework.Tools.FirmwareFlasher.Extensions
             }
             return result;
         }
+
+        //public static async Task<ParserResult<T>> WithNotParsedAsync<T>(this Task<ParserResult<T>> task, Action<IEnumerable<Error>> action)
+        //{
+        //    var result = await task;
+        //    if (result is NotParsed<T> notParsed)
+        //    {
+        //        return result.WithNotParsed(action);
+        //    }
+        //    return result;
+        //}
+
+        //public static async Task<ParserResult<T>> WithNotParsedAsync<T>(this ParserResult<T> result, Func<IEnumerable<Error>, Task> errorFunc)
+        //{
+        //    if (result is NotParsed<T> notParsed)
+        //    {
+        //        await errorFunc(notParsed.Errors);
+        //    }
+        //    return result;
+        //}
+
+        /// <summary>
+        ///     Async version of the WithNotParsed taking a Task as input for dot-appending to the WithParsedAsync extension method.
+        /// </summary>
+        /// <typeparam name="T">Type of the target instance built with parsed value.</typeparam>
+        /// <param name="task">A Task of <see cref="CommandLine.ParserResult{T}"/>.</param>
+        /// <param name="errorFunc">The <see cref="Func{<IEnumerable<Error>, Task}"/> to execute.</param>
+        /// <returns>The same <paramref name="task"/> instance as a <see cref="Task"/> instance.</returns>
+        public static async Task<ParserResult<T>> WithNotParsedAsync<T>(this Task<ParserResult<T>> task, Func<IEnumerable<Error>, Task> errorFunc)
+        {
+            var result = await task;
+            if (result is NotParsed<T> notParsed)
+            {
+                await errorFunc(notParsed.Errors);
+            }
+            return result;
+        }
+
     }
 }
