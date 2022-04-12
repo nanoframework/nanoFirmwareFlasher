711156dc133349e991855c8d909a1fb172ac1f8c
Robin Jones <robin@networkfusion.co.uk>
2022-04-12 22:20:46 +0100

    Add unit test skeleton

---
+++ nanoFirmwareFlasher.Tests/CloudsmithApiTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nanoFirmwareFlasher.Tests
+{
+    [TestClass]
+    public class CloudsmithApiTests
+    {
+        [TestMethod]
+        public void CheckApiUriExists()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void CheckApiVersionExists()
+        {
+            // And is not deprecated?!
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void DecodePackageDetails()
+        {
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void DecodePackageInformation()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
