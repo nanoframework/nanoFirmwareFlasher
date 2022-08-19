832ce5687a060086aa4efcac983667367f5b1a40
José Simões <jose.simoes@eclo.solutions>
2022-08-19 16:03:16 +0100

    Clean-up sample

--- Samples/ListJLinkDevicesApp/Program.cs
+++ Samples/ListJLinkDevicesApp/Program.cs
@@ -3,12 +3,8 @@
 // See LICENSE file in the project root for full license information.
 ////
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using nanoFramework.Tools.FirmwareFlasher;
+using System;
 
 namespace ListJLinkDevicesApp
 {
