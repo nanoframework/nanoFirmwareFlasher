0a8e1b3a96559dc4e55251f0a1256140c29272cc
Robin Jones <networkfusion@users.noreply.github.com>
2022-12-13 23:56:45 +0000

    Update nanoFirmwareFlasher.Library/Exceptions/StLinkCliExecutionException.cs
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- nanoFirmwareFlasher.Library/Exceptions/StLinkCliExecutionException.cs
+++ nanoFirmwareFlasher.Library/Exceptions/StLinkCliExecutionException.cs
@@ -39,7 +39,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
         /// STM32 Programmer CLI Exception.
         /// </summary>
         /// <param name="message">Message to display.</param>
-        /// <param name="innerException">The exception to display</param>
+        /// <param name="innerException">The exception to display.</param>
         public StLinkCliExecutionException(string message, Exception innerException) : base(message, innerException)
         {
         }
