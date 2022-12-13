5ce178849aff5c114c4bf0b4e67c0d62aa91a22a
Robin Jones <networkfusion@users.noreply.github.com>
2022-12-13 23:55:33 +0000

    Update nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
+++ nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
@@ -41,7 +41,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
         /// ESP32 tool flash exception.
         /// </summary>
         /// <param name="info">Serialized information.</param>
-        /// <param name="context">Streamed context</param>
+        /// <param name="context">Streamed context.</param>
         protected ReadEsp32FlashException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
