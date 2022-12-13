d8db78de9aaa5b2330178789bf99a17f2dec19b7
Robin Jones <networkfusion@users.noreply.github.com>
2022-12-13 23:42:34 +0000

    Update nanoFirmwareFlasher.Library/Exceptions/CantConnectToDfuDeviceException.cs
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- nanoFirmwareFlasher.Library/Exceptions/CantConnectToDfuDeviceException.cs
+++ nanoFirmwareFlasher.Library/Exceptions/CantConnectToDfuDeviceException.cs
@@ -32,7 +32,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
         /// <summary>
         /// DFU device connection exception.
         /// </summary>
-        /// <param name="message">Message to display</param>
+        /// <param name="message">Message to display.</param>
         /// <param name="innerException">The exception to display</param>
         public CantConnectToDfuDeviceException(string message, Exception innerException) : base(message, innerException)
         {
