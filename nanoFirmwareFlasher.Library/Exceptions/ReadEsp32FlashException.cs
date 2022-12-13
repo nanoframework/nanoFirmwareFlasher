6c11125feff083950ea7ef60546417ebc9a3e38d
Robin Jones <networkfusion@users.noreply.github.com>
2022-12-13 23:54:38 +0000

    Update nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
    Co-authored-by: José Simões <jose.simoes@eclo.solutions>

--- nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
+++ nanoFirmwareFlasher.Library/Exceptions/ReadEsp32FlashException.cs
@@ -32,7 +32,7 @@ namespace nanoFramework.Tools.FirmwareFlasher
         /// ESP32 tool flash exception.
         /// </summary>
         /// <param name="message">Message to display.</param>
-        /// <param name="innerException">The exception to display</param>
+        /// <param name="innerException">The exception to display.</param>
         public ReadEsp32FlashException(string message, Exception innerException) : base(message, innerException)
         {
         }
