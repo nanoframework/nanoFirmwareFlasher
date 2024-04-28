3ebe36c741b0ec4ca97f61164c8f3b77c32464a6
Cory Charlton <CoryCharlton@users.noreply.github.com>
2024-04-28 01:50:42 -0700

    Revert "PSRAM discovery is now performed for all targets (#264)" (#266)

--- nanoFirmwareFlasher.Library/EspTool.cs
+++ nanoFirmwareFlasher.Library/EspTool.cs
@@ -246,10 +246,19 @@ namespace nanoFramework.Tools.FirmwareFlasher
                 // FEATHER_S2's have PSRAM, so don't even bother
                 psramIsAvailable = PSRamAvailability.Yes;
             }
+            else if (name.Contains("ESP32-C3")
+                     || name.Contains("ESP32-S3"))
+            {
+                // all ESP32-C3/S3 SDK config have support for PSRAM, so don't even bother
+                psramIsAvailable = PSRamAvailability.Undetermined;
+            }
             else
             {
                 // if a target name wasn't provided, check for PSRAM
-                if (targetName == null)
+                // except for ESP32_C3 and S3
+                if (targetName == null
+                    && !name.Contains("ESP32-C3")
+                    && !name.Contains("ESP32-S3"))
                 {
                     psramIsAvailable = FindPSRamAvailable();
                 }
